fd3c11d824a1aee4a2fd4b820abb7ed3a97344b1
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-11 15:07:38 -0400

    Parent class School and its constructor method

---
+++ main.js
@@ -0,0 +1,8 @@
+class School {
+    constructor(name, level, numbeOfStudents) {
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+
+}

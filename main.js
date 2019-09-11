a461db5245d3476ff1bca2b3414adaae6da7b8a9
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-11 15:11:40 -0400

    getter and setter for class School

--- main.js
+++ main.js
@@ -4,5 +4,24 @@ class School {
         this._level = level;
         this._numberOfStudents = numberOfStudents;
     }
+    get name() {
+        return this._name;
+    }
+
+    get level() {
+        return this._level;
+    }
+
+    get numberOfStudents() {
+        return this._numberOfStudents;
+    }
+
+    set numberOfStudents(input) {
+        if (input.isNaN()) {
+            console.log('Invalid input: numberOfStudents must be set to a Number.');
+        } else {
+            this._numberOfStudents = input;
+        }
+    }
 
 }
